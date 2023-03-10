f21ff657a4b0c2ac6a57af3ca4e1754fc9e564aa
Oscar Bastidas <bastidas2000oscar@gmail.com>
2023-03-10 18:29:34 -0500

    Agregar archivos de proyecto.

---
+++ obastidast/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="obastidast.MvcApplication" Language="C#" %>
---
+++ obastidast/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(obastidast.Startup))]
+namespace obastidast
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
